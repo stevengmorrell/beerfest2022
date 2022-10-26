bd56e3fea341df608f93daf5eb85897e1819a062
Bumbag <42703220+Bumbag@users.noreply.github.com>
2022-10-26 21:46:22 +0100

    more styling

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name='description' content='Beer Guide 2022' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/images/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/images/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/images/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/images/site.webmanifest' />
+
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,31 +4,52 @@ import {
   Container,
   CssBaseline,
   Grid,
+  ThemeProvider,
   Typography,
 } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import Head from "next/head";
+import Image from "next/image";
 import React from "react";
 import BeerCard from "../components/BeerCard";
-import Heading from "../components/Heading";
+
 import beerData from "../json/beerData.json";
 
 export default function Home() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Roboto", '"Helvetica Neue"', "Arial", "sans-serif"].join(
+        ","
+      ),
+    },
+  });
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>West Bridgford Hockey Club Beer Festival 2022</title>
+      </Head>
       <CssBaseline />
-      <Heading />
-      <Container sx={{ backgroundColor: "#FFC40A" }}>
+      <Container sx={{ backgroundColor: "#FFC40A", paddingBottom: "30px" }}>
         <Box>
           <Card sx={{ marginBottom: "20px" }}>
+            <Box sx={{ width: "200px", height: "200px", position: "relative" }}>
+              <Image
+                src={"/images/WBHCLogo.jpg"}
+                alt={"WBHC Logo"}
+                layout='fill'
+                objectFit='contain'
+              />
+            </Box>
             <Typography variant='h4'>WBHC Beer Festival 2022</Typography>
+          </Card>
+          <Card>
+            <Typography>
+              Add date, location (link to maps), no entry fee{" "}
+            </Typography>
           </Card>
 
           <Box>
-            <Grid
-              container
-              spacing={2}
-              // columnSpacing={{ xs: 0, sm: 1, md: 3 }}
-              // rowSpacing={{ xs: 1, sm: 2, md: 3 }}
-            >
+            <Grid container spacing={2}>
               {beerData.map((beer) => (
                 <Grid item key={beer.id}>
                   <BeerCard
@@ -46,6 +67,6 @@ export default function Home() {
           </Box>
         </Box>
       </Container>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
